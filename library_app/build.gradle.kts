5456f575b541a7ed5cbad1d8f2af42eb67f9bd52
Novikov Artem <novikovaa6@student.bmstu.ru>
2025-09-17 14:46:26 +0300

    Add lab_02

--- library_app/build.gradle.kts
+++ library_app/build.gradle.kts
@@ -1,7 +1,32 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.allure)
+}
+
+subprojects {
+    apply(plugin = "io.qameta.allure")
+
+    tasks.register<Delete>("cleanAllureReport") {
+        delete(layout.buildDirectory.dir("reports/allure-report/allureReport"))
+    }
+
+    tasks.register<Copy>("copyAllureHistory") {
+        from(layout.buildDirectory.dir("reports/allure-report/allureReport/history"))
+        into(layout.buildDirectory.dir("allure-results/history"))
+        doFirst {
+            println("[${project.name}] Copying Allure history from last report...")
+        }
+    }
+
+    tasks.withType<Test>().configureEach {
+        dependsOn("copyAllureHistory")
+    }
+
+    tasks.named("allureReport") {
+        dependsOn("cleanAllureReport")
+        dependsOn(tasks.withType<Test>())
+    }
 }
